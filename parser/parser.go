36f5acb6498a1d3cabd17826d6534212b03e14d0
Tiago Flora <tiagomflora@gmail.com>
2023-09-27 14:03:15 -0700

    Add integer parsing

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"interpreter-mod/ast"
 	"interpreter-mod/lexer"
 	"interpreter-mod/token"
+	"strconv"
 )
 
 // define constants for precedence, only order matters
@@ -39,6 +40,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	p.nextToken()
 	p.nextToken()
@@ -173,4 +175,18 @@ func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("Could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
